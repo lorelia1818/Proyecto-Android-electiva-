a0bf6000663e4de771e7bd2689e150cad52a1920
lorelia1818 <laroca94@uan.edu.co>
2022-10-31 15:35:43 -0500

    commit en login

--- app/src/main/java/edu/uan/juegodepuzzombis/Login.java
+++ app/src/main/java/edu/uan/juegodepuzzombis/Login.java
@@ -1,17 +1,28 @@
 package edu.uan.juegodepuzzombis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
     EditText correoLogin, passLogin;
     Button BTNLogin;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,7 @@ public class Login extends AppCompatActivity {
         correoLogin = findViewById(R.id.correoLogin);
         passLogin = findViewById(R.id.passLogin);
         BTNLogin = findViewById(R.id.BTNLogin);
+        auth = FirebaseAuth.getInstance();
 
         BTNLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,10 +48,33 @@ public class Login extends AppCompatActivity {
                     passLogin.setError("Contraseña debe ser mayor a 6 ");
                     passLogin.setFocusable(true);
                 }else{
-                    //LogeoDeJugador(email,pass);
+                    LogeoDeJugador(email,pass);
                 }
             }
         });
 
+    }
+
+    private void LogeoDeJugador(String email, String pass) {
+        auth.signInWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser user = auth.getCurrentUser();
+                            startActivity(new Intent(Login.this,Menu.class));
+                            assert user != null; //afirmamos que el usuario no es nulo 
+                            Toast.makeText(Login.this, "BIENVENIDO(A)"+user.getEmail(), Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+
+                    }
+                    // si es que falla el logueo , muestra un mensaje
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
